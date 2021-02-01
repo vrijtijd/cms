0702aa0a1baae388bf9e114f9a319eb6e22afca7
Gerard O'Neill <grardb@gmail.com>
2021-01-31 22:37:16 -0800

    Cascade deletes on team_user table for both users and teams

---
+++ database/migrations/2021_02_01_062610_update_team_user_table_to_cascade_on_delete.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTeamUserTableToCascadeOnDelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('team_user', function (Blueprint $table) {
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('team_user', function (Blueprint $table) {
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+}
